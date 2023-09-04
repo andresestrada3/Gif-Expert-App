20a25f4080cb0f827b1367839371ebae6853142e
Andres Estrada <andres.estrada3@udea.edu.co>
2023-09-04 12:21:53 -0500

    Actualizando estilos

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async ( category ) => {
     const apiKey = 'KLQO2Z1VYPl3Hjn3jqNHNz82uILRH3GC';
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=12`;
     const resp = await fetch( url );
     const {data} = await resp.json();
     
